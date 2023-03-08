8ebf16e4b1ade27d7dafe47328e02c73819011b1
Nikita <rogozhin.nickita2018@yandex.ru>
2023-03-08 19:05:23 +0300

    new

---
+++ seven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class seven
+{
+    public static void main(String[] args)
+    {
+        try(Scanner sc = new Scanner(System.in))
+        {
+            int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+            if (a + b < c || a + c < b || b + c < a)
+                System.out.println("No");
+            else
+                System.out.println("Yes");
+        }
+    }
+}
